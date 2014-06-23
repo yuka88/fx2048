ad124d03fe5c63c403498897d9ade1f1529b150a
sonia <sonia@sonia-Aspire-6930G>
2014-06-23 16:25:21 +0200

    Creazione interfaccia GiocatoreAutomatico come nelle specifiche

---
+++ src/giocatoreAutomatico/GiocatoreAutomatico.java
@@ -0,0 +1,23 @@
+package giocatoreAutomatico;
+
+import game2048.*;
+
+
+/**
+ * @author Sonia Cannas
+ */
+public interface GiocatoreAutomatico {
+    
+     /** restituisce un oggetto GiocatoreAutomatico su cui si potrà chiedere che mosse fare.  
+     Lancia una eccezione se c'e' un problema nella creazione del giocatore automatico */
+    public static GiocatoreAutomatico getGiocatoreAutomatico() throws Exception {
+            return (GiocatoreAutomatico) Class.forName("giocatoreAutomatico.player.MyGiocatoreAutomatico").newInstance();
+    }
+
+    /**   restituisce 0=ALTO; 1=DX; 2=BASSO; 3=SX , ovvero la mossa che il giocatore automatico intende fare.
+     * In input prende una griglia di gioco 4x4 che contiene la situazione del gioco corrente.
+     * 
+     * @see Griglia
+     */
+    public abstract int prossimaMossa(Griglia g);
+}
