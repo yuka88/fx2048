ca00a98f410d5945bc86bae68ef9d1a2affb4eb8
sonia <sonia@sonia-Aspire-6930G>
2014-06-23 16:24:38 +0200

    Creazione classe MyGiocatoreAutomatico come nelle specifiche

---
+++ src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package giocatoreAutomatico.player;
+
+import game2048.*;
+import giocatoreAutomatico.GiocatoreAutomatico;
+import giocatoreAutomatico.Griglia;
+
+/**
+ *
+ * @author sonia
+ */
+public class MyGiocatoreAutomatico implements GiocatoreAutomatico {
+    
+    public int prossimaMossa(Griglia g){
+        return 0;
+    }
+    
+}
