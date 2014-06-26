5a186c7f0d70acd9423a663382a73be124963161
sonia <sonia@sonia-Aspire-6930G>
2014-06-26 17:58:26 +0200

    Implementazione metodo prossimaMossa (sbagliato)

--- src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
+++ src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
@@ -7,17 +7,49 @@
 package giocatoreAutomatico.player;
 
 import game2048.*;
+import static game2048.Direction.*;
 import giocatoreAutomatico.GiocatoreAutomatico;
 import giocatoreAutomatico.Griglia;
+import java.util.Random;
 
 /**
  *
  * @author sonia
  */
 public class MyGiocatoreAutomatico implements GiocatoreAutomatico {
+    private GameManager gameManager;
+    
+    /**   restituisce 0=ALTO; 1=DX; 2=BASSO; 3=SX , ovvero la mossa che il giocatore automatico intende fare.
+     * In input prende una griglia di gioco 4x4 che contiene la situazione del gioco corrente.
+     * 
+     * @see Griglia
+     */
+    
+    public MyGiocatoreAutomatico(GameManager gameManager){
+            this.gameManager = gameManager;
+    }
+    
     
     public int prossimaMossa(Griglia g){
-        return 0;
+        Griglia grigliaNuova = new MyGriglia();
+        Random rand = new Random();
+        int mossa;
+        do{
+            mossa = rand.nextInt(3);
+            if(mossa==0) {
+                gameManager.move(UP);
+            }
+            if(mossa==1) {
+                gameManager.move(RIGHT);
+            }
+            if(mossa==2) {
+                gameManager.move(DOWN);
+            }
+            if(mossa==3) {
+                gameManager.move(LEFT);
+            }
+        }while(grigliaNuova.equals(g));
+        return mossa;
     }
     
 }
