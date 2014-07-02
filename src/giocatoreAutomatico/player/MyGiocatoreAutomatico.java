a4bc653c8b5350159ba26c9b63112841872e7c6c
sonia <sonia@sonia-Aspire-6930G>
2014-07-02 10:57:56 +0200

    Modifica implementazione metodo gameHelp()

--- src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
+++ src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
@@ -34,36 +34,49 @@ public class MyGiocatoreAutomatico implements GiocatoreAutomatico {
         boolean fail = true;
         do{
             mossa = rand.nextInt(3);
-            if(mossa==2){
+            
+            if(mossa==0){ //mossa UP (sale)
                 for(int i=0; i<=2; i++)
                     for(int j=0; j<=3; j++){
                         Location loc = new Location(i,j);
-                        if(g.get(loc)==-1)
-                            fail = false;
+                        if(g.get(loc)==-1){
+                            for(int k=3; k>i; k--)
+                                if(g.get(new Location(k,j))!=-1)
+                                    fail = false;
+                        }
                     }
             }
-            if(mossa==1){
-                for(int i=0; i<=3; i++)
-                    for(int j=0; j<=2; j++){
-                        Location loc = new Location(i,j);
-                        if(g.get(loc)==-1)
-                            fail = false;
-                    }
-            }
-            if(mossa==0){
-                for(int i=1; i<=3; i++)
-                    for(int j=0; j<=3; j++){
-                        Location loc = new Location(i,j);
-                        if(g.get(loc)==-1)
-                            fail = false;
-                    }
-            }
-            if(mossa==3){
+            if(mossa==1){ //mossa RIGHT
                 for(int i=0; i<=3; i++)
                     for(int j=1; j<=3; j++){
                         Location loc = new Location(i,j);
-                        if(g.get(loc)==-1)
-                            fail = false;
+                        if(g.get(loc)==-1){
+                            for(int k=0; k<j; k++)
+                                if(g.get(new Location(i,k))!=-1)
+                                    fail = false;
+                        }
+                    }
+            }
+            if(mossa==2){ //mossa DOWN (scende)
+                for(int i=1; i<=3; i++)
+                    for(int j=0; j<=3; j++){
+                        Location loc = new Location(i,j);
+                        if(g.get(loc)==-1){
+                            for(int k=0; k<i; k++)
+                                if(g.get(new Location(k,j))!=-1)
+                                    fail = false;
+                        }
+                    }
+            }
+            if(mossa==3){ //mossa LEFT 
+                for(int i=0; i<=3; i++)
+                    for(int j=0; j<=2; j++){
+                        Location loc = new Location(i,j);
+                        if(g.get(loc)==-1){
+                            for(int k=3; k>j; k--)
+                                if(g.get(new Location(i,k))!=-1)
+                                    fail = false;
+                        }
                     }
             }
         }while(fail);
