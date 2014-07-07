e61745a10ffcabe915f764305c8f12f9b32831fd
ubuntu <ubuntu@ubuntu-VirtualBox>
2014-07-07 15:15:53 +0200

    documentazione Javadoc

--- src/giocatoreAutomatico/Griglia.java
+++ src/giocatoreAutomatico/Griglia.java
@@ -8,12 +8,10 @@ package giocatoreAutomatico;
 
 /**
  *
- * @author sonia
+ * @author Sonia Cannas e Noemi Sarritzu
+ * Interfaccia che definisce la griglia del gioco
  */
 
-/** le classi che implementano questa interfaccia devono contenere esattamente 16 chiavi.
-Alla Location (0,0), dovrà essere associato il numero (2, oppure 4, oppure 8, ...) associato a quella casella.
-Qualora nella posizione non ci siano numeri, sarà associato il valore -1
-*/
+
 public interface Griglia extends java.util.Map<game2048.Location,Integer> { }
 
