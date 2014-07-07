b66fb56e39923298bccb83173f875a630a8e3ef3
ubuntu <ubuntu@ubuntu-VirtualBox>
2014-07-07 15:16:04 +0200

    documentazione Javadoc

--- src/giocatoreAutomatico/MyGriglia.java
+++ src/giocatoreAutomatico/MyGriglia.java
@@ -10,10 +10,14 @@ import game2048.*;
 
 /**
  *
- * @author ubuntu
+ * @author Sonia Cannas e Noemi Sarritzu
+ * Classe che implementa l'interfaccia Griglia
  */
 public class MyGriglia extends HashMap<Location,Integer> implements Griglia{
     private HashMap<Location,Integer> griglia;
+    /**
+     *costruisce una nuova griglia
+     */
     public MyGriglia(){
         this.griglia= new HashMap<Location,Integer>();
     }
