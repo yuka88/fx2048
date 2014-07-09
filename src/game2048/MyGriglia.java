eea233ef529fe2de8a37c716d034d85b19595409
ubuntu <ubuntu@ubuntu-VirtualBox>
2014-07-09 18:32:42 +0200

    documentazione Javadoc

---
+++ src/game2048/MyGriglia.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package game2048;
+import java.util.HashMap;
+import game2048.*;
+import giocatoreAutomatico.Griglia;
+
+/**
+ *
+ * @author Sonia Cannas e Noemi Sarritzu
+ * Classe che implementa l'interfaccia Griglia
+ */
+public class MyGriglia extends HashMap<Location,Integer> implements Griglia{
+    private HashMap<Location,Integer> griglia;
+    /**
+     *costruisce una nuova griglia
+     */
+    public MyGriglia(){
+        this.griglia= new HashMap<Location,Integer>();
+    }
+           
+                    
+                 
+               
+   
+    
+    }
+    
